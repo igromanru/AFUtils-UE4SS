4fbabd15956cc4663425bad2b00cc2a81c916bcb
igromanru <igromanru@yahoo.de>
2024-08-28 14:45:32 +0200

    feat: Start CharacterSkills enum

--- AFUtils.lua
+++ AFUtils.lua
@@ -55,6 +55,28 @@ AFUtils.GearInventoryIndex = {
     OffHandShield = 11
 }
 
+---@enum CharacterSkills
+AFUtils.CharacterSkills = {
+    None = 0,
+    NewEnumerator0 = 1,
+    NewEnumerator1 = 2,
+    NewEnumerator2 = 3,
+    NewEnumerator3 = 4,
+    NewEnumerator4 = 5,
+    NewEnumerator5 = 6,
+    NewEnumerator6 = 7,
+    NewEnumerator7 = 8,
+    NewEnumerator8 = 9,
+    NewEnumerator9 = 10,
+    NewEnumerator10 = 11,
+    NewEnumerator11 = 12,
+    NewEnumerator12 = 13,
+    NewEnumerator13 = 14,
+    NewEnumerator14 = 15,
+    NewEnumerator15 = 16,
+    NewEnumerator16 = 17
+}
+
 ---Map of weather events
 ---@type { [string]: string }
 AFUtils.WeatherEvents = {
